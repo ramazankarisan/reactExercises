449711cd174e668e71413a19b182d785e86cf394
ramazankarisan <rkarisan25@gmail.com>
2021-11-25 21:23:06 +0100

    Add component created and separated

---
+++ note-exercise-add-edit/src/components/Add.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Add(props,) {
+  return (
+    <form onSubmit={props.onSubmit} >
+      <input onChange={props.onChange} type="text" name="text" value={props.text.title} />
+      <button type="submit">Add to List</button>
+    </form>
+  )
+}
+
+export default Add
